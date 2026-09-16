ac2d18f11ee7974cb876c57acb1bc7f77d816327
The-First-Snake <apophisxxxxx@gmail.com>
2026-09-17 01:03:18 +0530

    Vulnerable code running on my hopes, tapes and dreams. One liner

--- complex.c
+++ complex.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct Complex{
-  int real = 0;
-  int complex = 0;
+  int real;
+  int complex;
 }Complex;
 
-int addition(){
-  printf("For addition of complex numbers.\n");
-  printf("Each argument must be entered in the format 'a' or 'bi' where a and b are integers.\n Enter end to stop putting arguments\n");
-  int a = 0; int b = 0; int n = 0; char input[50] = "Empty"; char *end;
-  while(n==0){
-    scanf("49%s",input);
-    n = strtol(input, &end, 10);
-    if (strcmp(end, "i") == 0){
-      if(n == 0)
-      n = 1;
-      b = b + n;
-      printf("+");
-      n = 0;
-     }
-    else if (strcmp(end, "") == 0){
-      a = a + n;
-      printf("+");
-      n = 0;
-     }
-    else if (strcmp(end, "end") == 0)
-      n++;
-    else{
-      printf("Wrong input. Showing results for the rest if you have any.\n");
-      n++;
-     }
-    }
-  printf("%d+%di\n", a, b);
+int print(Complex A){
+  char *x = "+";
+  char result[50] = "";
+  if (A.complex<0)
+    x = "";
+  char compA[24] = ""; char compB[24] = "";
+  snprintf(compA, sizeof(compA), "%d", A.real);
+  snprintf(compB, sizeof(compB), "%d", A.complex);
+  strcpy(result, "");
+  strcat(result, compA);
+  strcat(result, x);
+  strcat(result, compB);
+  strcat(result, "i");
+  printf("%s\n",result);
   return 0;
 }
 
-Complex real(char input[]){
+Complex extract(char input[]){
   Complex values; int consumed = 0; int a = 0; int b = 0;
     if (sscanf(input, "%d+%di%n", &a, &b, &consumed) == 2 && input[consumed] == '\0'){
     values.real = a; values.complex = b;
@@ -60,45 +48,51 @@ Complex real(char input[]){
   return values;
 }
 
+int addition(){
+  printf("For addition of complex numbers.\n");
+  printf("Each argument must be entered in the format 'a' or 'bi' where a and b are integers.\n Enter end to stop putting arguments\n");
+  Complex compo; Complex output; char input[50] = "Empty";
+  while(true){
+    scanf("%49s",input);
+    if (strcmp(input, "end") != 0){
+    compo = extract(input);
+    output.real = output.real + compo.real;
+    output.complex = output.complex + compo.complex;
+    }
+    else if (strcmp(input, "end") == 0)
+      break;
+    else{
+      printf("Wrong input. Showing results for the rest if you have any.\n");
+     }
+    }
+  print(output);
+  return 0;
+}
+
 int multiplication(){
-  printf("For multiplication of complex numbers. \n");
-  printf("Arguments must be passed one at a time. And in the format a+bi, a+ib or a or bi.\n Enter end to stop putting arguments.\n");
-  char output[50] = "Empty"; char input[50] = "Empty"; char compA[25] = ""; char compB[25] = "";
-  int a = 0; int b = 0; int c = 0; int d = 0; int x = 0; int y = 0;
-  fgets(output, sizeof(input), stdin);
+  printf("\nFor multiplication of complex numbers. \n");
+  printf("Arguments must be passed one at a time. And in the format a+bi, a+ib or a or bi.\nEnter end to stop putting arguments.\n");
+  char output[50] = "Empty"; char input[50] = "Empty"; Complex part1; Complex part2; Complex part3;
+  fgets(output, sizeof(output), stdin);
   output[strcspn(output, "\n")] = '\0';
+  part1 = extract(output);
   while(true){
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
     if (strcmp(input, "end") != 0){
-      a = real(output);
-      b = complex(output);
-      c = real(input);
-      d = complex(input);
-      x = (a*c)-(b*d);
-      y = (a*d)+(b*c);
-      snprintf(compA, sizeof(compA), "%d", x);
-      snprintf(compB, sizeof(compB), "%d", y);
-      strcpy(output, "");
-      strcat(output, compA);
-      strcat(output, "+");
-      strcat(output, compB);
-      strcat(output, "i");
+      part2 = extract(input);
+      part3.real = (part1.real*part2.real)-(part1.complex*part2.complex);
+      part3.complex = (part1.real*part2.complex)+(part1.complex*part2.real);
+      part1 = part3;
      }
     else if (strcmp(input, "end") == 0)
       break;
     else
       exit(1);
    }
-
-  printf("%s",output);
+  print(part1);
   return 0;
-
 }
-
-int output(Complex A){
-
-
 
 int main(){
   multiplication();
